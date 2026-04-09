535e704556ac3faba2a6d10a1c0acccbbe8e3179
F <bfra11638@gmail.com>
2026-04-09 23:53:13 +0200

    Configurazione delle Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import Game from './components/Game'
+import { Link, Route, Routes } from 'react-router';
+import Game from './components/Game';
+import InfiniteGame from './components/InfiniteGame';
 
 function App() {
+
+  function Home() {
+    return (
+      <>
+        <nav className='game-menu'>
+            <Link to="/classic">Classic</Link>
+            <Link to="/infinite">Infinite</Link>
+        </nav>
+      </>
+    )
+  }
+
   return (
     <>
-      <Game />
+      <Routes>
+        <Route path='/' element={Home} />
+        <Route path='/classic' element={<Game />} />
+        <Route path='/infinite' element={<InfiniteGame />} />
+      </Routes>
     </>
   )
 }
